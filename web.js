e5b1f8cd124af9bdf299962e416d90dff0dc6e3d
Manju M <meghwani.manju@gmail.com>
2013-07-10 12:22:37 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
+function get_data_from_index_file(file) {
+  var bindata = fs.readFileSync(file);
+  return new Buffer(bindata).toString();
+}
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var file = 'index.html';
+  var data = get_data_from_index_file(file);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
